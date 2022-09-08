418b72ae14828d583b52730b3a4ca3ce8e603742
Charooo05 <charleschuks99@gmail.com>
2022-09-08 15:37:46 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-	printf("The size of double is: %ld - byte\n", sizeof(double));
-	printf("The size of float is: %ld - byte\n", sizeof(float));
-	printf("The size of char is: %ld - byte\n", sizeof(char));
-	printf("The size of int is: %ld - byte\n", sizeof(int));
+	printf("size of double is: %ld - byte\n", sizeof(double));
+	printf("size of float is: %ld - byte\n", sizeof(float));
+	printf("size of char is: %ld - byte\n", sizeof(char));
+	printf("size of int is: %ld - byte\n", sizeof(int));
+	printf("Size of a long long int: %ld - byte\n", sizeof(long long int));
+	printf("size of a long int is: %ld - byte\n", sizeof(long int));
 	return (0);
 }
