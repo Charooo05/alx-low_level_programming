8b4e5a628443aeb2b17b87bfda65c3306965aa02
Charooo05 <charleschuks99@gmail.com>
2022-09-08 15:30:39 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("The size of double is: %ld - byte\n", sizeof(double));
+	printf("The size of float is: %ld - byte\n", sizeof(float));
+	printf("The size of char is: %ld - byte\n", sizeof(char));
+	printf("The size of int is: %ld - byte\n", sizeof(int));
+	return (0);
+}
