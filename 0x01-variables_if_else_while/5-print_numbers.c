3e06876ba12b2c8ae42f55e7097fd71866f5090d
Charooo05 <charleschuks99@gmail.com>
2022-09-09 17:00:44 -0700

    task 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int base10;
+
+	for (; base10 < 10 ; base10++)
+		printf("%d", base10);
+	return (0);
+}
