27ef70f046c84262474a1902251c266827af6247
Charooo05 <charleschuks99@gmail.com>
2022-09-15 09:17:18 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,16 @@ void more_numbers(void)
 {
 	int n, c;
 
-	for (n = 0; n <= 14; n++)
+	for (c = 1; c <= 10; c++)
 	{
-		if (n > 9)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar((n / 10) + 48);
+			if (n > 9)
+			{
+				_putchar((n / 10) + 48);
+			}
+			_putchar((n % 10) + 48);
 		}
-		_putchar((n % 10) + 48);
 	}
 	_putchar('\n');
 }
