9c3bf053905167ef4580ff2ba45b6909aa99409a
Charooo05 <charleschuks99@gmail.com>
2022-09-19 15:00:16 -0700

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * puts_half -  prints half of a string
+ *
+ * @str: string parameter
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	char c;
+
+	for (i = 0; str[i]; i++)
+	{
+		c = str[i];
+		if (i >= (_strlen(str) / 2))
+		_putchar(c);
+	}
+	_putchar('\n');
+}
