e79279d316b2bf8286853abb1cc325800cad33f7
Charooo05 <charleschuks99@gmail.com>
2022-09-19 11:38:45 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to an int parameter & update the value to 98
+ *
+ * @n: pointer variable
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	int a;
+
+	n = &a;
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	void reset_to_98(int *n);
+	return (0);
+}
