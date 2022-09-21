44cb55c5391cadab409f7e83413ff2a30555892d
Charooo05 <charleschuks99@gmail.com>
2022-09-21 12:40:46 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ *
+ * @dest: main string
+ * @src: string to be appended
+ *
+ * Return: resulting string
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; src[i]; i++)
+	{
+	}
+		for (j = 0; dest[i]; j++)
+		{
+			dest[i + j] = src[j];
+		}
+	dest[i + j] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char *_strcat(char *dest, char *src);
+	return (0);
+}
