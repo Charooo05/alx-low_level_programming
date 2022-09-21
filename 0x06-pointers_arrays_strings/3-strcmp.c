cbac11ad5ec4346ca24670d142254b3af02875da
Charooo05 <charleschuks99@gmail.com>
2022-09-21 16:28:52 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,15 +14,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, n;
 
-	for (i = 0; s1[i] && s2[i]; i++)
-	{
-		n = s1[i] - s2[i];
-
-		if (n != 0)
-		return (n);
-
-		else if (s1[i] || s2[i])
-			return (0);
-	}
+	i = 0;
+	n = s1[i] - s2[i];
 	return (n);
 }
