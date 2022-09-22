26aa94815760169d9a798abca75c15bb081ee361
Charooo05 <charleschuks99@gmail.com>
2022-09-22 12:02:56 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,11 +14,14 @@ char *cap_string(char *a)
 
 	for (i = 0; a[i]; i++)
 	{
+		if (i == 0 && (a[i] >= 'a' && a[i] <= 'z'))
+				a[i] = a[i] - 32;
+
 		if (a[i] >= 'A' && a[i] <= 'z')
 		{
 			continue;
 		}
-		else if (a[i] < 48 || a[i] > 122)
+		else if (a[i] < 48 || a[i] > 122 || a[i] == '\t')
 		{
 			if (a[i + 1] >= 'a' && a[i + 1] <= 'z')
 				a[i + 1] = a[i + 1] - 32;
