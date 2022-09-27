17ea2cd88a2cd3174b0ab306fd89d3617d94ea30
Charooo05 <charleschuks99@gmail.com>
2022-09-27 14:18:13 -0700

    task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ *
+ * @a: chessboard pieces
+ *
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = 0;
+	while (i < 8)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+		i++;
+	}
+}
