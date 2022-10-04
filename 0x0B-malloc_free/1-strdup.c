05d01b1136ac7fd2dbcc67f0056d3a6464b48b3e
Charooo05 <charleschuks99@gmail.com>
2022-10-04 13:27:18 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,17 +15,17 @@ char *_strdup(char *str)
 {
 	char *s;
 	unsigned int i;
-	
+
 	if (str == NULL)
 		return (NULL);
 
 	s = malloc((strlen(str) * sizeof(char)) + 1);
-	
+
 	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; str[i]; i++)
 		s[i] = str[i];
-	
+
 	return (s);
 }
