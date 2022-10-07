00dfc3561060909fe7853ee5ef9eef5db5ed51bf
Charooo05 <charleschuks99@gmail.com>
2022-10-06 17:20:11 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -23,7 +23,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (n == NULL)
 		return (NULL);
 
-	for (i = 0; i <= nmemb; i++)
+	for (i = 0; i <= nmemb * size; i++)
 		n[i] = 0;
 
 	return (n);
