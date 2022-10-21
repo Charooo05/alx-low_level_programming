937a0ae96e9a8c2d4e96258479438d1e3f0015d4
Charooo05 <charleschuks99@gmail.com>
2022-10-21 15:55:28 -0700

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * function_before_main - prints statement before running main func
  */
 
-void function_before_main(void) __attribute__((constructor))
+void __attribute__((constructor)) function_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
