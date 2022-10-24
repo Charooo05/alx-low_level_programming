cbe4a2cdfd1191f45ff3aab95172aec1a2a9d609
Charooo05 <charleschuks99@gmail.com>
2022-10-24 15:30:26 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (i <= idx)
 		{
-			if (i == idx)
+			if (i == idx - 1)
 			{
 				ptr->next = h->next;
 				h->next = ptr;
